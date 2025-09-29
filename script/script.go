82ca2e4c623e3c2b1429ec258d484a23ce48772b
ranggakrisnaa <infinity.duos@gmail.com>
2025-09-29 11:03:05 +0700

    update script kafka

--- script/script.go
+++ script/script.go
@@ -11,6 +11,9 @@ func Script(scriptName string, db *gorm.DB) error {
 	case "example_script":
 		exampleScript := NewExampleScript(db)
 		return exampleScript.Run()
+	case "kafka_setup":
+		kafkaScript := NewKafkaScript()
+		return kafkaScript.Run()
 	default:
 		return errors.New("script not found")
 	}
